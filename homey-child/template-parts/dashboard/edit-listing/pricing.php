5b5429db40f264c29f3ac9a8b9f56d4d71d3416a
Sadiq <muhammadsadiq037@gmail.com>
2022-08-27 15:27:31 +0500

    text correction

--- homey-child/template-parts/dashboard/edit-listing/pricing.php
+++ homey-child/template-parts/dashboard/edit-listing/pricing.php
@@ -306,7 +306,7 @@ if($hide_fields['price_postfix'] != 1) {
             <div class="row">
                <div class="col-sm-6 col-xs-12">
                  <div class="form-group">
-                      <label for="show_hide" style="width:100%"><?php echo esc_html__('are you want to show the price','homey'); ?></label>
+                      <label for="show_hide" style="width:100%"><?php echo esc_html__('do you want to show the price','homey'); ?></label>
                          <input type="radio" name="yes_no" <?php checked( $price_no, 'yes' ); ?> value="yes">
                          <label for="yes">YES</label>
                          <input type="radio"  name="yes_no" <?php checked( $price_no, 'no' ); ?> value="no">
